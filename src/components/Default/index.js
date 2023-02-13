71c22e01f9d4abb0cb4e5d3103af838c54ca0368
Fernando Machado <107931700+FernandoDevJava@users.noreply.github.com>
2023-02-12 21:50:21 -0300

    Update index.js

--- src/components/Default/index.js
+++ src/components/Default/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import LogoMobato from './assets/mobato_logo2.png'
+import MachadoLogo from './assets/Machado-logo.png'
 import './styles.css'
 const Default = () => {
   return (
     <div className="Container-Default">
-        <img src={LogoMobato} alt="" />
+        <img src={MachadoLogo} alt="" />
         <h1>Chat Mobato</h1>
         <span>
             Agora você pode enviar e receber mensagens sem precisar manter seu celular conectado à internet.
